eaf3003a427aa535466975fc008de92e71fb4fa8
bhushiss <kit58163@gmail.com>
2024-06-14 15:28:43 +0700

    first commit

---
+++ lab2-1.py
@@ -0,0 +1,5 @@
+score = int (input("รับค่าทําคะเเนน: :"))
+if score >= 60:
+    print("ผ่าน")
+else:
+    print("ไม่ผ่าน")
---
+++ lab2-2.py
@@ -0,0 +1,13 @@
+score = int(input("รับค่าคะเเนน: "))
+if score >=80 and score <=100:
+       print("เกรดA")
+elif score >=70 and score <=79:
+       print("เกรดB")
+elif score >=60 and score <=69:
+       print("เกรดc")
+elif score >=50 and score <=59:
+       print("เกรดD")
+elif score >=0 and score <=49:
+       print("เกรดF")
+else:
+    print("กรุณากรอกข้อมูล 0-100")
